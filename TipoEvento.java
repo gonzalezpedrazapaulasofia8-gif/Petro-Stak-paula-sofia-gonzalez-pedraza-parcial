86c1d470063d0e32cbd33826e89fcd16981a0fe6
paula sofia gonzalez pedraza <gonzalezpedrazapaulasofia8@gmail.com>
2026-04-24 22:03:52 -0500

    Archivo tipo evento

---
+++ TipoEvento.java
@@ -0,0 +1,14 @@
+/*Enumeración de tipos válidos: FUGA, MANTENIMIENTO, LECTURA_PRESION, CIERRE_TURNO. */
+
+package JavaAplicacion;
+
+public class TipoEvento {
+    public static final String FUGA = "FUGA";
+    public static final String MANTENIMIENTO = "MANTENIMIENTO";
+    public static final String LECTURA_PRESION = "LECTURA_PRESION";
+    public static final String CIERRE_TURNO = "CIERRE_TURNO";
+}
+   
+
+    
+
